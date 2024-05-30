f217a0c7737409b63ce3fe02dc9010fbf52f089c
khushal vora <vorakhushal482@gmail.com>
2024-05-30 14:22:58 +0530

    add

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,18 @@
-import React, { useState, useContext } from 'react';
-// import { AuthContext } from './AuthContext';
-import { Link,useNavigate  } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import authService from './services/authService';
-// import { AuthContext } from '../context/AuthContext.js';
+
 const Login = () => {
- 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const navigate  = useNavigate ();
+  const navigate = useNavigate();
 
-  // const { setIsLoggedIn } = useContext(AuthContext);
-  // Function to handle form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
-   
+
     try {
       const response = await authService.login({ email, password });
-
 
       if (!response.ok) {
         throw new Error('Login failed');
@@ -26,9 +21,6 @@ const Login = () => {
       const data = await response.json();
       localStorage.setItem('authToken', data.token);
       navigate('/dashboard');
-
-      // setIsLoggedIn(true);
-      // window.location.href = '/';
     } catch (error) {
       setErrorMessage('Login failed. Please check your credentials.');
     }
@@ -38,7 +30,9 @@ const Login = () => {
     <div className="auth-main">
       <div className="auth_div vivify fadeIn">
         <div className="auth_brand">
-          <a className="navbar-brand" href="/home"><img src='assets/images/icon.svg' alt="aa" width={50} className='d-inline-block align-top mr-2' />Mooli</a>
+          <a className="navbar-brand" href="/home">
+            <img src='assets/images/icon.svg' alt="aa" width={50} className='d-inline-block align-top mr-2' />Mooli
+          </a>
         </div>
         <div className="card">
           <div className="header">
