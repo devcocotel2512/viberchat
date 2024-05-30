840e5e0cf62e8c56ac2ff4ed5707e54c02bd8c82
devcocotel <devcocotel@gmail.com>
2024-05-30 16:03:49 +0530

    login

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,33 +1,47 @@
 // Routes.js
-import React,{useContext,useState} from 'react';
-import { Route, Routes  } from 'react-router-dom';
+import React,{useContext,useState,useEffect} from 'react';
+import { Route, Routes ,Navigate  } from 'react-router-dom';
 import Home from '../pages/Home';
 import Setting from '../pages/Setting';
 import Contact from '../pages/Contact';
 import Login from '../pages/Login';
 import Chat from '../pages/Chat';
 import Register from '../pages/Register';
-import Chennel from '../pages/Chennel';
+import Channel from '../pages/Channel';
 import AddUser from '../pages/AddUser';
 
 
 
 function RoutesComponent() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  // const [loggedIn, setLoggedIn] = useState(false);
 
-  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    // Check for existing token in local storage on app load
+    const token = localStorage.getItem('authToken');
+    
+    if (token) {
+      setIsLoggedIn(true);
+    }
+}, []);
 
   return (
     
     <Routes>
-      <Route path="/home"  element={<Home />} />
-      <Route path="/setting" element={<Setting />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/chenel" element={<Chennel />} />
-      <Route path="/add-user" element={<AddUser />} />
+      <Route path="/"  element={ isLoggedIn ? <Home /> : <Navigate to="/login" replace />} />
+      {/* <Route path="/login"  element={<Login />} /> */}
+      <Route path="/login"  element={ isLoggedIn ? <Navigate to="/" replace /> : <Login />} />
+      <Route path="/register"  element={<Register />} />
+      
+      <Route path="/home"  element={ isLoggedIn ? <Home /> : <Navigate to="/login" replace />} />
+      <Route path="/setting" element={ isLoggedIn ? <Setting /> : <Navigate to="/login" replace />} />
+      <Route path="/contact" element={ isLoggedIn ? <Contact /> : <Navigate to="/login" replace />}  />
+      
+      <Route path="/chat" element={ isLoggedIn ? <Chat /> : <Navigate to="/login" replace />}  />
+      
+      <Route path="/channel" element={ isLoggedIn ? <Channel /> : <Navigate to="/login" replace />}  />
+      <Route path="/add-user" element={ isLoggedIn ? <AddUser /> : <Navigate to="/login" replace />}  />
+      
     </Routes>
     
   );
