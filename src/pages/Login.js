844ee15d0d73554477cefa0d8239ed9b71d67ac4
khushal vora <vorakhushal482@gmail.com>
2024-05-30 10:27:45 +0530

    a

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext.js';
 const Login = () => {
-  const { setIsLoggedIn } = useAuth();
-
+ 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
